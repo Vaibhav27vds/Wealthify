e3159ef392607750d99ff6423d79d55f0d7b2e19
Vaibhav Deep Srivastava <Vaibhav27vds@gmail.com>
2024-05-15 15:20:57 +0530

    added dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,9 +2,10 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { Dashboard } from "@/components/dashboardOne";
+import { DashboardMain } from "@/components/dashboardMain";
 
 export default function Home() {
   return (
-    <Dashboard />
+    <DashboardMain />
   );
 }
