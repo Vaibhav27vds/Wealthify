db62346f45d1deae74688137b04eb3e6f89c1fcb
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-05-15 19:49:00 +0530

    commented an issue

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { UserButton} from "@clerk/nextjs";
-import { Dashboard } from "@/components/SettingsDashboard";
+// import { Dashboard } from "@/components/SettingsDashboard";
 import { DashboardMain } from "@/components/MainDashboard";
 
 
