079f9f0d7e4f62360aa7e672d6583974c8931e21
Vaibhav Deep Srivastava <Vaibhav27vds@gmail.com>
2024-05-15 14:37:35 +0530

    dashboardOne added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
+import { Dashboard } from "@/components/dashboardOne";
 
 export default function Home() {
   return (
-    <UserButton />
+    <Dashboard />
   );
 }
---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+
+export const runtime = 'edge';
+
+const app = new Hono().basePath('/api')
+
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello Next.js!',
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
